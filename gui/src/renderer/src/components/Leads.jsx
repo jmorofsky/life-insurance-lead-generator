6ae1e7339a99b3aff4990f39b9c7defdbcbb4379
jmorofsky <jmorofsky@gmail.com>
2026-06-12 01:52:10 -0400

    move Spinner to separate component

--- gui/src/renderer/src/components/Leads.jsx
+++ gui/src/renderer/src/components/Leads.jsx
@@ -3,6 +3,7 @@ import {
     MaterialReactTable,
     useMaterialReactTable,
 } from 'material-react-table';
+import Spinner from './Spinner';
 import Github from '@uiw/react-color-github';
 import ColorResetIcon from '../assets/colorReset.svg';
 import CloseIcon from '../assets/close.svg';
@@ -205,50 +206,9 @@ export default function LeadTable({ data, onColorChange }) {
     });
 
     if (isRendering) {
-        return (
-            <div className='m-auto'>
-                <svg
-                    class='animate-spin size-10'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                >
-                    <defs>
-                        <linearGradient
-                            id='spinner-gradient'
-                            x1='0%'
-                            y1='0%'
-                            x2='100%'
-                            y2='100%'
-                        >
-                            <stop
-                                offset='0%'
-                                stopColor='oklch(70.7% 0.165 254.624)'
-                            />
-
-                            <stop
-                                offset='100%'
-                                stopColor='oklch(70.2% 0.183 293.541)'
-                            />
-                        </linearGradient>
-                    </defs>
-
-                    <circle
-                        className='opacity-25'
-                        cx='12'
-                        cy='12'
-                        r='10'
-                        stroke='#9ca3af'
-                        strokeWidth='4'
-                    />
-
-                    <path
-                        className='opacity-75'
-                        fill={`url(#spinner-gradient)`}
-                        d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-                    />
-                </svg>
-            </div>
-        );
+        return <div className='m-auto'>
+            <Spinner size={45} />
+        </div>;
     };
 
     return <MaterialReactTable table={table} />;
